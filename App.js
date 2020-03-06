22f77b871be6de996f3d1abb8d1a7c0ef0efca1b
Zhylinskyi Roman <formicrotech@gmail.com>
2020-03-06 02:01:54 +0200

    ya vse razvernyl.

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Fedya tyt nado kakie-to bukvi pisat', shob vse zarabotalo - sho delat' ? :)</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ npm install expo-cli --global
 npm install
 cd ReactNetSpeedTest 
 expo start
+
+end then in apropriate console window type 'w' to enable developer mode for web browser .
